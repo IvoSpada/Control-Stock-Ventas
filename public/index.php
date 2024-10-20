2bd5a71508e70be047e80db66ece1626fefc718f
SalvaReyna <salvabusiness9@gmail.com>
2024-10-20 17:47:45 -0300

    Dashbord agregado

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 //lamar al router y los Controladores
 use MVC\Router;
 use Controllers\LoginController;
-
+use Controllers\AdminController;
 $router = new Router();
 
 //Seleccion de usuarios
@@ -14,6 +14,9 @@ $router->get('/', [LoginController::class, 'SelectUser']);
 //Login de Admin
 $router->get('/login', [LoginController::class, 'login']);
 $router->post('/login', [LoginController::class, 'login']);
+//Dashbord admin 
+$router->get('/admin/dashbord', [AdminController::class, 'admin']);
+$router->post('/admin/dashbord', [AdminController::class, 'admin']);
 
 //logout
 $router->get('/logout', [LoginController::class, 'logout']);
--- views/layout.php
+++ views/layout.php
@@ -6,7 +6,7 @@
     <title>Control de Stock y Ventas</title>
     <link rel="icon" href="./"><!--  Icono de HB -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="./build/css/styles.css">
+    <link rel="stylesheet" href="/build/css/styles.css">
 </head>
 <body>
     <?php echo $contenido; ?>
