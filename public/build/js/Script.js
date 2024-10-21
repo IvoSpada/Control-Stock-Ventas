ac5b7576b7f231cabd143f0eef404ca0c2c2c669
Ruiz Ignacio <164880710+IgnaciogRuiz@users.noreply.github.com>
2024-10-21 14:27:13 -0300

    Resuelto conflictos entre develop e IvoSpada-develop

--- public/build/js/Script.js
+++ public/build/js/Script.js
@@ -1,25 +1,50 @@
 function toggleDropdown() {
-    const dropdown = document.getElementById('dropdownMenu');
-    dropdown.classList.toggle('show');
+  const dropdown = document.getElementById("dropdownMenu");
+  dropdown.classList.toggle("show");
 }
 
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.classList.toggle("show");
+}
 
-  function toggleDropdown() {
-      var dropdownContent = document.getElementById("dropdownContent");
-      dropdownContent.classList.toggle("show");
-  }
-
-  // Cerrar el dropdown si se hace clic fuera de él
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-button')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+// Cerrar el dropdown si se hace clic fuera de él
+window.onclick = function (event) {
+  if (!event.target.matches(".dropdown-button")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
 
+/* CODIGO PARA EL ASIDE RESPONSIVE EN MOBILE */
+/* EL CODIGO COMENTADO ES UNA FUNCIONALIDAD EN DESAROLLO PERO AHORA MISMO IMPOSIBLE DE TESTEAR, 
+SE PUEDE VOLVER A ACTIVAR CUANDO SE PONGAN LOS LINKS EN LOS BOTONES */
 
+function toggleSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("show"); // Añade o quita la clase 'show'
+}
+
+// Cerrar el dropdown si se hace clic fuera de él
+// window.onclick = function (event) {
+//     if (!event.target.matches(".dropdown-button")) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         for (var i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains("show")) {
+//                 openDropdown.classList.remove("show");
+//             }
+//         }
+//     }
+
+//     // Cerrar la barra lateral si se hace clic fuera de ella
+//     const sidebar = document.querySelector(".sidebar");
+//     if (sidebar.classList.contains("show") && !event.target.matches(".toggle-sidebar")) {
+//         sidebar.classList.remove("show");
+//     }
+// };
